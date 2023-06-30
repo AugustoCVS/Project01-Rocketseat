40dac3aae6289122840d209ffcb7ee3a7a78bb3a
AugustoCVS <guty.vertolis@gmail.com>
2023-06-30 19:03:57 -0300

    style: start sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react';
 import { Header } from './components/Header/Header';
 import './css/global.css'
 
+import styles from './css/App.module.css'
+import { Sidebar } from './components/Sidebar/Sidebar';
+
 function App() {
   return (
     <div>
       <Header/>
-      <h1>Hello Word</h1>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <h2>coisa</h2>
+          <p>mais coisa</p>
+        </main>
+      </div>
     </div>
   )
 }
