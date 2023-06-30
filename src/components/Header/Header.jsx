ca749db3b8de77132b69103705d7c302f5020849
AugustoCVS <guty.vertolis@gmail.com>
2023-06-30 18:32:57 -0300

    style: create header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/igniteLogo.svg';
+
 export function Header(){
     return(
         <header className={styles.header}>
+            <img src={igniteLogo} alt="logo da ignite"/>
             <strong>Ignite Feed</strong>
         </header>
     );
