79b2bdd1f953b5dd66e8090252392a8c35085673
AugustoCVS <guty.vertolis@gmail.com>
2023-07-01 16:54:56 -0300

    feat: create new comment function

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,36 @@ import { Sidebar } from './components/Sidebar/Sidebar';
 import './css/global.css'
 import styles from './css/App.module.css'
 
+const posts =[
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/AugustoCVS.png',
+      name: 'Augusto Santana',
+      role: 'Intern Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera'},
+      {type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-07-01 15:52:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/AugustoCVS.png',
+      name: 'Larissa Cavalcante',
+      role: 'Mediciner'
+    },
+    content: [
+      {type: 'paragraph', content: 'Oii pessoal'},
+      {type: 'paragraph', content: "Acabei de estudar mais um pouco sobre Biologia🚀"},
+    ],
+    publishedAt: new Date('2023-06-29 16:00:00')
+  }
+]
+
 
 function App() {
   return (
@@ -14,7 +44,13 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post/>
+          {posts.map(post => {
+            return (<Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />)
+          })}
         </main>
       </div>
     </div>
--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css'
 import { Avatar } from '../Avatar/Avatar';
 
-export function Comment(){
+export function Comment({content}){
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/AugustoCVS.png"/>
@@ -19,7 +19,7 @@ export function Comment(){
                             <Trash size={24}/>
                         </button>
                     </header>
-                    <p>Muito bom parabens</p>
+                    <p>{content}</p>
                 </div>
 
 
